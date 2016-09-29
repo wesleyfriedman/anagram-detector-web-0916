3d1bc188ad0471223cfe8ec14729af83028162ae
wesleyfriedman <friedman.wesley@gmail.com>
2016-09-29 15:40:32 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(anagram_array)
+        new_array = anagram_array.map do |anagram_word|
+            if anagram_word.split("").sort == word.split("").sort
+                anagram_word
+            end
+        end
+        new_array.compact
+    end
+end
